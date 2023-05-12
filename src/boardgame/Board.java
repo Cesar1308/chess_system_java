168c01152acc8a7586a6154be35ae7df998b5886
Cesar1308 <120032687+Cesar1308@users.noreply.github.com>
2023-05-11 22:14:40 -0300

    Chess layer and printing the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -12,6 +12,30 @@ public class Board {
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
 	}
+
+	public int getRows() {
+		return rows;
+	}
+
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+
+	public int getColumns() {
+		return columns;
+	}
+
+	public void setColumns(int columns) {
+		this.columns = columns;
+	}
 	
+	// vai retornar a matriz pieces na linha row e na coluna column
+	public Piece piece(int row, int column) {
+		return pieces[row][column];
+	}
 	
+	//sobrecarga do metodo 
+	public Piece piece(Position position) {
+		return pieces[position.getRow()][position.getColumn()];
+	}
 }
